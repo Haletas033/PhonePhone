cc352330d68fe4a2b98eebba518f1aa45515df0f
Alex Curran <haletas88@gmail.com>
2025-10-28 22:57:30 +1300

    Language selection (currently not fully working)

---
+++ include/languages.h
@@ -0,0 +1,33 @@
+//
+// Created by halet on 10/28/2025.
+//
+
+#ifndef LANGUAGES_H
+#define LANGUAGES_H
+
+#define ENGLISH 0
+#define CHINESE 1
+#define GERMAN 2
+#define ITALIAN 3
+#define FRENCH 4
+#define SPANISH 5
+#define RUSSIAN 6
+#define JAPANESE 7
+#define KOREAN 8
+#include <string>
+#include <vector>
+
+inline std::vector<std::string> languages = {
+    "English",
+    "Chinese",
+    "German",
+    "Italian",
+    "French",
+    "Spanish",
+    "Russian",
+    "Japanese",
+    "Korean"
+
+};
+
+#endif //LANGUAGES_H
